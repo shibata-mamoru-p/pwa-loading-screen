5d4708b002e4afe95c5340e87ee4621095bc92b2
mamoru <shibata.mamoru.p@gmail.com>
2018-07-08 19:12:29 +0900

    add Main screen and transition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Loading from './Loading';
+import Main from './Main';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      loaded: false
+    };
+
     this.onLoadingFinished = this.onLoadingFinished.bind(this);
   }
 
   onLoadingFinished() {
-    console.log("LOADING DONE!");
+    this.setState({ loaded: true });
   }
 
   render() {
     return (
-      <div className="App">
-        <Loading onLoadingFinished={ this.onLoadingFinished } />
-      </div>
+      <TransitionGroup className="App">
+        <CSSTransition
+          key={ this.state.loaded }
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={'fade'}>
+            { this.state.loaded
+              ? <Main />
+              : <Loading onLoadingFinished={ this.onLoadingFinished } /> }
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Main">
+        <p>Welcome</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
